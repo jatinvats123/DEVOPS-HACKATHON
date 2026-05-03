d9cdca8d285eb7f5e085239c572b917d8aa9ecfc
Gaurav <anmulwadgaurav@gmail.com>
2026-05-04 02:54:02 +0530

    feat: implement monitoring API service and dashboard page skeleton

--- Frontend/src/features/monitoring/services/monitor.api.js
+++ Frontend/src/features/monitoring/services/monitor.api.js
@@ -1,6 +1,5 @@
-import {env} from '../../../config/env.js';
-import { apiRequest } from '../../../lib/api/apiRequest.js';
-
+import { env } from "../../../config/env.js";
+import { apiRequest } from "../../../lib/api/apiRequest.js";
 
 /*   
  * @getIncidentsByMonitorId - Fetches incidents for a specific monitor by its ID.
@@ -8,30 +7,22 @@ import { apiRequest } from '../../../lib/api/apiRequest.js';
 
 */
 
-
 export const createMonitoring = async (monitorData) => {
-
-  return apiRequest("post", env.CREATE_MONITORING_API, monitorData)
-}
-
-
-
-
-/* 
-*@getMonitors - Fetches all monitors for the authenticated user.
-*/
-export const getMonitors = async () => {
-    return apiRequest("get", env.CREATE_MONITORING_API);
-}
-
-
+  return apiRequest("post", env.CREATE_MONITORING_API, monitorData);
+};
 
 /*
-*@deleteMonitor - Deletes a monitor by its ID.
-*@param {string} monitorId - The ID of the monitor to delete.   
-*/
+ *@getMonitors - Fetches all monitors for the authenticated user.
+ */
+export const getMonitors = async () => {
+  return apiRequest("get", env.CREATE_MONITORING_API);
+};
+
+/*
+ *@deleteMonitor - Deletes a monitor by its ID.
+ *@param {string} monitorId - The ID of the monitor to delete.
+ */
 
 export const deleteMonitor = async (monitorId) => {
-    return apiRequest("delete", `${env.CREATE_MONITORING_API}/${monitorId}`);
-}
-    
+  return apiRequest("delete", `${env.CREATE_MONITORING_API}/${monitorId}`);
+};
